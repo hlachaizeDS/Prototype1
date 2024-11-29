6ad26bc7a101d1e763be820052364eabd978bcd6
Mouaadh KELLAL <mouaadh.kellal@dnascript.co>
2024-11-29 20:18:35 +0300

    Calculate relative positions vs reference, create unit tests

--- hardware/mga/movement.py
+++ hardware/mga/movement.py
@@ -13,40 +13,56 @@ def get_movement_range_coordinates(
     if routine is None or len(routine.positionThresholdToStateMapping) == 0:
         return None
 
+    start_coordinate = find_alignment_coordinates(geometry, alignment)
+
     sign = 1 if routine.direction == routine.Direction.forward else -1
-    start_x = (
+    start_x = start_coordinate.x + (
         routine.positionThresholdToStateMapping[0].positionThreshold - sign * margin.x
     )
-    end_x = (
+    end_x = start_coordinate.x + (
         routine.positionThresholdToStateMapping[-1].positionThreshold + sign * margin.x
     )
+    y = start_coordinate.y
 
-    y = geometry.reference.position.y + (
-        alignment.row
-        - alignment.nozzle_index * geometry.y_inter_nozzle_spacing_wells_in_line
-    )
-
-    return Coordinate(0 * start_x, y), Coordinate(end_x, y)
+    return Coordinate(start_x, y), Coordinate(end_x, y)
 
 
-def find_first_nozzle_on_first_well_coordinate(
+def find_alignment_coordinates(
     geometry: Geometry,
     alignment: Alignment,
 ):
+    reference_manifold_index = (
+        geometry.reference.line_index // geometry.number_of_lines_in_manifold
+    )
+    alignment_manifold_index = (
+        alignment.line_index // geometry.number_of_lines_in_manifold
+    )
+    manifold_index_difference = alignment_manifold_index - reference_manifold_index
+
+    reference_line_index = (
+        geometry.reference.line_index % geometry.number_of_lines_in_manifold
+    )
+    alignment_line_index = alignment.line_index % geometry.number_of_lines_in_manifold
+
     return Coordinate(
         x=geometry.reference.position.x
         + (
-            (alignment.line_index - geometry.reference.line_index)
+            (alignment_line_index - reference_line_index)
+            * geometry.inter_line_spacing_wells
+            - geometry.reference.well.column
+            + (alignment.nozzle_index - geometry.reference.nozzle_index)
             * geometry.x_inter_nozzle_spacing_wells_in_line
-            + geometry.reference.well.column
         )
         * geometry.inter_well_spacing_mm,
         y=geometry.reference.position.y
         + (
-            (
-                alignment.row - geometry.reference.well.row
-                + alignment.nozzle_index * geometry.y_inter_nozzle_spacing_wells_in_line
+            (alignment.row - geometry.reference.well.row)
+            + (
+                alignment.nozzle_index
+                - geometry.reference.nozzle_index
+                + manifold_index_difference * geometry.number_of_nozzles_per_line
             )
-            * geometry.inter_well_spacing_mm
-        ),
+            * geometry.y_inter_nozzle_spacing_wells_in_line
+        )
+        * geometry.inter_well_spacing_mm,
     )
--- hardware/mga/test/test_movement.py
+++ hardware/mga/test/test_movement.py
@@ -1,36 +1,109 @@
 import unittest
 from hardware.mga.movement import (
     get_movement_range_coordinates,
-    find_first_nozzle_on_first_well_coordinate,
+    find_alignment_coordinates,
 )
 from hardware.mga.configuration import StandardGeometry, Coordinate
 from hardware.mga.dispense import Routine, Alignment
+from hardware.mga.types import Well
 
 
 class TestMovement(unittest.TestCase):
-    geometry = StandardGeometry
+    def test_get_movement_range_coordinates(self):
+        geometry = StandardGeometry
+        geometry.reference.well = Well(row=6, column=0)
+        geometry.reference.position = Coordinate(200.0, 100.0)
+        geometry.reference.line_index = 0
+        geometry.reference.nozzle_index = 3
 
-    # def test_get_movement_range_coordinates(self):
-    #     alignment = Alignment(line_index=0, nozzle_index=3, row=0)
-    #     routine = Routine(
-    #         direction=Routine.Direction.forward,
-    #         positionThresholdToStateMapping=[
-    #             Routine.Item(positionThreshold=0, state={}),
-    #             Routine.Item(positionThreshold=10, state={}),
-    #         ],
-    #     )
-    #     coordinates = get_movement_range_coordinates(
-    #         self.geometry, alignment, routine, Coordinate(2, 0)
-    #     )
-
-    #     self.assertIsNone(coordinates)
-    #     if coordinates is not None:
-    #         self.assertTupleEqual(coordinates, (Coordinate(-2, 0), Coordinate(12, 0)))
-
-    def test_find_first_nozzle_on_first_well_coordinate(self):
-        alignment = Alignment(line_index=0, nozzle_index=3, row=0)
-        coordinate = find_first_nozzle_on_first_well_coordinate(
-            self.geometry, alignment
+        alignment = Alignment(line_index=0, nozzle_index=3, row=6)
+        routine = Routine(
+            direction=Routine.Direction.forward,
+            positionThresholdToStateMapping=[
+                Routine.Item(positionThreshold=0, state={}),
+                Routine.Item(positionThreshold=10, state={}),
+            ],
+        )
+        coordinates = get_movement_range_coordinates(
+            geometry, alignment, routine, Coordinate(2, 0)
         )
 
-        self.assertEqual(coordinate, Coordinate(0.0, 0.0))
+        self.assertIsNotNone(coordinates)
+        if coordinates is not None:
+            self.assertTupleEqual(coordinates, (Coordinate(198.0, 100.0), Coordinate(212.0, 100.0)))
+
+    def test_find_alignment_coordinates_reference(self):
+        geometry = StandardGeometry
+        geometry.reference.well = Well(row=6, column=0)
+        geometry.reference.position = Coordinate(10.0, 100.0)
+        geometry.reference.line_index = 0
+        geometry.reference.nozzle_index = 0
+
+        alignment = Alignment(line_index=0, nozzle_index=3, row=0)
+        coordinate = find_alignment_coordinates(geometry, alignment)
+
+        self.assertEqual(coordinate, Coordinate(13.0, 100.0))
+
+    def test_find_alignment_coordinates_different_line_to_reference(self):
+        geometry = StandardGeometry
+        geometry.reference.well = Well(row=6, column=0)
+        geometry.reference.position = Coordinate(300.0, 10.0)
+        geometry.reference.line_index = 2
+        geometry.reference.nozzle_index = 3
+
+        alignment = Alignment(line_index=5, nozzle_index=3, row=6)
+        coordinate = find_alignment_coordinates(geometry, alignment)
+
+        self.assertEqual(coordinate, Coordinate(327.0, 10.0))
+
+    def test_find_alignment_coordinates_different_nozzle_to_reference(self):
+        geometry = StandardGeometry
+        geometry.reference.well = Well(row=8, column=0)
+        geometry.reference.position = Coordinate(200.0, 100.0)
+        geometry.reference.line_index = 2
+        geometry.reference.nozzle_index = 3
+
+        alignment = Alignment(line_index=2, nozzle_index=1, row=8)
+        coordinate = find_alignment_coordinates(geometry, alignment)
+
+        self.assertEqual(coordinate, Coordinate(198.0, 82.0))
+
+    def test_find_alignment_coordinates_different_row_to_reference(self):
+        geometry = StandardGeometry
+        geometry.reference.well = Well(row=4, column=0)
+        geometry.reference.position = Coordinate(200.0, 100.0)
+        geometry.reference.line_index = 3
+        geometry.reference.nozzle_index = 1
+
+        alignment = Alignment(line_index=3, nozzle_index=1, row=10)
+        coordinate = find_alignment_coordinates(geometry, alignment)
+
+        self.assertEqual(coordinate, Coordinate(200.0, 127.0))
+
+    def test_find_alignment_coordinates_different_nozzle_to_reference_different_manifolds(
+        self,
+    ):
+        geometry = StandardGeometry
+        geometry.reference.well = Well(row=6, column=0)
+        geometry.reference.position = Coordinate(200.0, 100.0)
+        geometry.reference.line_index = 6
+        geometry.reference.nozzle_index = 0
+
+        alignment = Alignment(line_index=0, nozzle_index=0, row=6)
+        coordinate = find_alignment_coordinates(geometry, alignment)
+
+        self.assertEqual(coordinate, Coordinate(200.0, 64.0))
+
+    def test_find_alignment_coordinates_different_line_row_nozzle_to_reference_different_manifolds(
+        self,
+    ):
+        geometry = StandardGeometry
+        geometry.reference.well = Well(row=0, column=0)
+        geometry.reference.position = Coordinate(200.0, 100.0)
+        geometry.reference.line_index = 11
+        geometry.reference.nozzle_index = 3
+
+        alignment = Alignment(line_index=0, nozzle_index=0, row=12)
+        coordinate = find_alignment_coordinates(geometry, alignment)
+
+        self.assertEqual(coordinate, Coordinate(152.0, 91.0))
