0994d75fd2b306a83ea67a882dd54f4e9a426590
OTF machine <mouaadh.kellal@dnascript.co>
2025-02-21 16:55:04 +0100

    Adjust movement margin to avoid missed dispenses towards the end

--- hardware/mga/test/test_hardware.py
+++ hardware/mga/test/test_hardware.py
@@ -10,15 +10,24 @@ def test_dispense():
     left_wells = [i for i in range(1, 193)]
     right_wells = [i for i in range(193, 385)]
 
+    checkered_positive = [
+        i for i in range(1, 385) if (i - 1) % 16 % 2 == (i - 1) // 16 % 2
+    ]
+    checkered_negative = [
+        i for i in range(1, 385) if (i - 1) % 16 % 2 != (i - 1) // 16 % 2  
+    ]
+
     test_hardware.initialisation()
     test_hardware.dispense(
         volume_per_line={
-            # "EB": (12.5, all_wells),
-            # "DB": (12.5, all_wells),
-            "A": (12,right_wells),
-            # "C": (25, right_wells),
-            #"G": (12, right_wells),
-            "T": (12, left_wells),
+            # "T": (25, checkered_positive),
+            # "A": (12, checkered_negative),
+            # "EB": (25, left_wells),
+            # "DB": (25, all_wells),
+            # "C": (12, left_wells),
+            "A": (12, left_wells),
+            # "G": (12, right_wells),
+            "T": (12, right_wells),
         }
     )
     test_hardware.move_to(Coordinate(x=0.0, y=0.0))
